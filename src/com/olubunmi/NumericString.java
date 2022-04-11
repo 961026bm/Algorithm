af948a972747498e8dc71e88c31494d0aed5ce47
961026bm <olubunmiosunde.student@careerdevs.com>
2022-04-11 19:25:16 -0400

    NumericString commit

---
+++ src/com/olubunmi/NumericString.java
@@ -0,0 +1,22 @@
+package com.olubunmi;
+
+public class NumericString {
+    public static String add(String a, String b) {
+        if (a == null || b == null || a.equals("") || b.equals(""))
+            return "Invalid Operation";
+        int num = Integer.parseInt(a);
+        int num1 = Integer.parseInt(b);
+        int num2 = num + num1;
+        System.out.println(num2);
+        return String.valueOf(num2);
+
+
+    }
+}
+
+/* public class NumericString {
+you need to parse the string to integer before adding
+You CANNOT ADD STRING
+then return it back to string with String.valueOf()
+}
+*/
