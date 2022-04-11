e0d5a080a0c85c9fe99cc5b44327bd6bd2c9c7bc
961026bm <olubunmiosunde.student@careerdevs.com>
2022-04-11 19:28:33 -0400

    NumericString solutiun with try catch commit

--- src/com/olubunmi/NumericString.java
+++ src/com/olubunmi/NumericString.java
@@ -2,14 +2,22 @@ package com.olubunmi;
 
 public class NumericString {
     public static String add(String a, String b) {
-        if (a == null || b == null || a.equals("") || b.equals(""))
-            return "Invalid Operation";
-        int num = Integer.parseInt(a);
+//        if (a == null || b == null || a.equals("") || b.equals(""))
+//            return "Invalid Operation";
+//        int num = Integer.parseInt(a);
+//        int num1 = Integer.parseInt(b);
+//        int num2 = num + num1;
+//        System.out.println(num2);
+//        return String.valueOf(num2);
+        try {
+            int num = Integer.parseInt(a);
         int num1 = Integer.parseInt(b);
         int num2 = num + num1;
         System.out.println(num2);
         return String.valueOf(num2);
-
+        }catch (Exception e) {
+            return "Invalid Operation";
+        }
 
     }
 }
