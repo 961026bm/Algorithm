e27faa10c83e7e6c850d8400b1fbd1b5e1c00cac
961026bm <olubunmiosunde.student@careerdevs.com>
2022-04-12 18:45:43 -0400

    area of a triangle commit

---
+++ src/com/olubunmi/TriArea.java
@@ -0,0 +1,12 @@
+package com.olubunmi;
+
+public class TriArea {
+    public static int triArea(int base, int height) {
+
+        int area = (base * height)/2;
+        return area;
+//System.out.println("Area of Triangle is: "+ area)
+
+
+    }
+}
