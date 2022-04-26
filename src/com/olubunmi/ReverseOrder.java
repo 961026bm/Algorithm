98f583006516fbaa3ee4c8185f50274238d96542
961026bm <olubunmiosunde.student@careerdevs.com>
2022-04-26 18:48:17 -0400

    ReverseOrder python solution commit

--- src/com/olubunmi/ReverseOrder.java
+++ src/com/olubunmi/ReverseOrder.java
@@ -2,8 +2,23 @@ package com.olubunmi;
 
 public class ReverseOrder {
     public static String reverse(final String str) {
-        StringBuilder sb=new StringBuilder(str);
-        sb.reverse();
-        return sb.toString();
+//        StringBuilder sb = new StringBuilder(str);
+//        sb.reverse();
+//        return sb.toString();
+        String result = "";
+        for(int i = str.length()-1; i >= 0; i--) {
+           result += str.charAt(i);
+
+        }
+          return result;
     }
+
 }
+/*
+Python solution
+def reverse(txt):
+	result = ""
+	for i in range(len(txt)-1, -1, -1):
+		result += txt[i]
+	return result;
+ */
