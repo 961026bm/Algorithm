aa2f4fcff801f7662521f3b326ae2e8483314dc3
961026bm <olubunmiosunde.student@careerdevs.com>
2022-04-27 19:39:57 -0400

    count boolean solution commit

--- src/com/olubunmi/TrueCount.java
+++ src/com/olubunmi/TrueCount.java
@@ -12,3 +12,20 @@ public class TrueCount {
         return trueCount;
     }
 }
+/*
+int count = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == true)
+				count++;
+		}
+		return count;
+
+		public class Challenge {
+  public static int countTrue(boolean[] arr) {
+    int coun = 0;
+    for (boolean i : arr) if (i) coun += 1;
+    return coun;
+  }
+}
+ */
